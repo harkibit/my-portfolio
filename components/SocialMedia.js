e0acbaf92e66ebb41bc90390c6c56ce2343865d6
Hiba Abdel Karim <abdelkarim.hiba@outlook.com>
2022-05-23 10:21:49 +0300

    added emailjs package and sendEmail function

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
+import emailjs from "@emailjs/browser";
+
+import { Heading, TextInput, TextArea, Button } from "grommet";
 
 import styles from "../styles/Home.module.css";
-import { Heading, TextInput, TextArea, Button } from "grommet";
+
 import twitter from "../public/images/twitter.png";
 import linkedin from "../public/images/linkedin.png";
 import outlook from "../public/images/outlook.png";
@@ -20,12 +23,34 @@ export default function SocialMedia() {
     content: "",
   });
 
+  const contactForm = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        contactForm.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className={styles.componentContainer} style={{ border: "none" }}>
       <div className={styles.randomFacts}>
         <div>
           <Heading>Let's Connect!</Heading>
-          <div>
+          <form ref={contactForm} onSubmit={sendEmail}>
             <TextInput
               placeholder="Enter your name"
               value={form.name}
@@ -52,59 +77,73 @@ export default function SocialMedia() {
               style={{ resize: "none" }}
               rows={6}
             />
-            <Button primary label="Get in touch!" className={styles.btn} />
-          </div>
+            <Button
+              primary
+              label="Get in touch!"
+              className={styles.btn}
+              type="submit"
+            />
+          </form>
         </div>
         <div className={styles.socialMediaContainer}>
           <Image
+            alt="twitter"
             src={twitter}
             width={100}
             height={100}
             className={styles.twitter}
           />
           <Image
+            alt="Linkedin"
             src={linkedin}
             width={100}
             height={100}
             className={styles.linkedin}
           />
           <Image
+            alt="outlook"
             src={outlook}
             width={100}
             height={100}
             className={styles.outlook}
           />
           <Image
+            alt="Instagram"
             src={instagram}
             width={100}
             height={100}
             className={styles.outlook}
           />
           <Image
+            alt="Youtube"
             src={youtube}
             width={100}
             height={100}
             className={styles.outlook}
           />
           <Image
+            alt="Zoom"
             src={zoom}
             width={100}
             height={100}
             className={styles.outlook}
           />
           <Image
+            alt="Discord"
             src={discord}
             width={100}
             height={100}
             className={styles.outlook}
           />
           <Image
+            alt="Slack"
             src={slack}
             width={100}
             height={100}
             className={styles.outlook}
           />
           <Image
+            alt="Dribbble"
             src={dribbble}
             width={100}
             height={100}
